e40a5b5feb8203cf831665419f270b31e4b213ee
ApriyantoWibowo <pri.yant0@ovi.com>
2016-11-02 16:13:57 +0700

    12141362 menambahkan output untuk mengisikan jumlah baris yang diinginkan dan mendeklarasikan baris sebagai inputan

--- src/nestedloop/NestedLoop.java
+++ src/nestedloop/NestedLoop.java
@@ -7,6 +7,8 @@ public class NestedLoop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int x,y,baris;
-         
+        System.out.println("jumlah baris : ");
+        baris = input.nextInt();
+             
     }
 }
