d85adb770166100155bb026438c249320ae08da8
ApriyantoWibowo <pri.yant0@ovi.com>
2016-11-02 16:10:17 +0700

    12141362 menambahkan fungsi Scanner pada program java segitiga bintang siku kiri bawah dan menambahkan variable x,y dan baris

--- src/nestedloop/NestedLoop.java
+++ src/nestedloop/NestedLoop.java
@@ -7,13 +7,6 @@ public class NestedLoop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int x,y,baris;
-        System.out.println("jumlah baris : ");
-        baris = input.nextInt();
-        for (x=0;x<baris;x++){
-            for (y=0;y<=x;y++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }     
+         
     }
 }
