e55b1f4c7339d5166263592a3ca38f929532473c
ApriyantoWibowo <pri.yant0@ovi.com>
2016-11-02 16:22:30 +0700

    12141362 menambahkan output untuk mengisikan jumlah baris yang diinginkan dan mendeklarasikan baris sebagai inputan

--- src/nestedloop/SikuKiriAtas.java
+++ src/nestedloop/SikuKiriAtas.java
@@ -6,6 +6,8 @@ public class SikuKiriAtas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int i,j,baris;
+        System.out.println("jumlah baris : ");
+        baris = input.nextInt();
         
         
     }
