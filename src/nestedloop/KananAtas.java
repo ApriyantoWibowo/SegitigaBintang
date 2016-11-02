81036ee1d2ab3be82ea84fc9177d36dc800a811c
ApriyantoWibowo <pri.yant0@ovi.com>
2016-11-02 16:41:56 +0700

    12141362 menambahkan fungsi for i dan for a, for i digunakan untuk membuat baris kebawah berdasarkan jumlah baris yang kita inputkan, for a digunakan untuk membuat baris kesamping dengan karakter *

--- src/nestedloop/KananAtas.java
+++ src/nestedloop/KananAtas.java
@@ -8,6 +8,11 @@ public class KananAtas {
         int i,j,baris;
         System.out.println("jumlah baris : ");
         baris = input.nextInt();
-        
+        for(i=0; i<baris; i++) {
+            for(int a=0; a<i; a++) {
+                System.out.print(" ");
+            }
+            
+        }
     }
 }
